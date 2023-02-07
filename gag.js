18275be70c8efe74073a367c11215711949309a7
BRYUSS <zubaydullamurodullayev4@gmail.com>
2023-02-07 21:36:05 +0500

    qo'shdim

--- gag.js
+++ gag.js
@@ -9,5 +9,24 @@ bert.value = date.join("")
 }
 
 function bool1() {
- date.pop()
+   date.pop([date.length-1])
+   bert.value = date.join("")
+
 }
+let check = document.querySelector(".check")
+let som = false
+function chek() {
+    let val =+bert.value
+   check.classList.add("qwert")
+   fif1.innerHTML = `summa:${val}so'm`
+   fif2.innerHTML =`kesh back :${(val/100)*1.25}so'm` 
+   console.log(check);
+}
+let card = document.querySelector(".karta");
+
+function bit(){
+    card.classList.toggle("post")
+
+}
+
+
